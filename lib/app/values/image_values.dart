d383d5bd0e2fc44253cac249b1a3e11c6f8027d3
Jevon Putra <jevonop88@gmail.com>
2024-12-17 12:00:55 +0700

    PHASE 1 - Add image resources

---
+++ lib/app/values/image_values.dart
@@ -0,0 +1,8 @@
+class ImageValues {
+  static const String icIllustration = 'assets/images/ic_doctor_and_nurse.svg';
+
+  static const String icCalendar = "assets/images/ic_calendar.svg";
+  static const String icPlaceholder = "assets/images/img_placeholder.png";
+
+  static const String icDropDown = "assets/images/ic_drop_down.svg";
+}
