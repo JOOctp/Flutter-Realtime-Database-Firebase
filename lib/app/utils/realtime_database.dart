4cf1f6f92497ff627ed55f3844ffca21ef53405a
Jevon Putra <jevonop88@gmail.com>
2024-12-23 13:49:18 +0700

    PHASE 1 - update logic check user is exist

--- lib/app/utils/realtime_database.dart
+++ lib/app/utils/realtime_database.dart
@@ -2,22 +2,17 @@ import 'package:firebase_database/firebase_database.dart';
 
 import '../data/patient_presence.dart';
 import '../data/room.dart';
-import '../data/user_data.dart';
 
 class RealtimeDatabase {
   static String USERS = "users";
   static String ROOMS = "rooms";
   static String PATIENT_PRESENCE = "patient_presence";
 
-  Future checkAndStoreUser({required UserData user}) async {
-    DatabaseReference databaseReference = FirebaseDatabase.instance.ref('$USERS/${user.uuid}');
+  Future<bool> isUserExist({required String email}) async {
+    DatabaseReference databaseReference = FirebaseDatabase.instance.ref(USERS);
     final data = await databaseReference.get();
 
-    if(data.exists){
-      return await databaseReference.update(user.toJson());
-    } else {
-      return await databaseReference.set(user.toJson());
-    }
+    return data.children.firstWhere((element) => element.value.toString() == email).exists;
   }
 
   //ROOM
